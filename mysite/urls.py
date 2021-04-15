0aed00e5736f186c3b4a7f6682a059ebd5cc67e8
Yazek13 <fedorov_d_l@mail.ru>
2021-04-15 16:44:41 +1000

    Configured routes urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from news.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('test/', test)
+    path('', include('news.urls')),
+
 ]
--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from news.views import index
+from news.views import *
 
 urlpatterns = [
     path('', index),
+    path('test/', test),
+
 ]
